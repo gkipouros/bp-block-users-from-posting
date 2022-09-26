4961a7a5e8c224a5d3714d0d25e93810813abab4
Giannis Kipouros <gkipouros@exis.com.gr>
2022-09-26 10:56:37 +0300

    Clean up

--- app/main/class-block-member-posting.php
+++ app/main/class-block-member-posting.php
@@ -35,10 +35,6 @@ if ( ! class_exists( 'BP_Block_Member_Posting' ) ) {
             // Enqueue front-end scripts
             add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_style_scripts' ), 100 );
 
-            add_action( 'bp_before_activity_loop', array( $this, 'display_feed_notices' ) );
-
-            add_action( 'wp_ajax_delete_pinned_feed_notice',
-                array( $this, 'delete_pinned_feed_notice' ) );
 
         }
 
@@ -71,7 +67,7 @@ if ( ! class_exists( 'BP_Block_Member_Posting' ) ) {
 
 
             // Provide a global object to our JS file containing the AJAX url and security nonce
-            wp_localize_script( 'bp-block-member-posting-custom-script', 'BPPfnAjaxObject',
+            wp_localize_script( 'bp-block-member-posting-custom-script', 'BPBMPAjaxObject',
                 array(
                     'ajax_url'   => admin_url( 'admin-ajax.php' ),
                     'ajax_nonce' => wp_create_nonce( 'ajax_nonce' ),
@@ -81,175 +77,7 @@ if ( ! class_exists( 'BP_Block_Member_Posting' ) ) {
 
         }
 
-        /**
-         * Display the feed notifications
-         */
-        public function display_feed_notices() {
 
-            // Bail out if not the main activity feed
-            if ( ! bp_is_directory() || ! bp_is_current_component( 'activity' ) ) {
-                return;
-            }
-
-            // Show only on the first page
-            if ( ! isset( $_POST['page'] ) || (int) $_POST['page'] != 1 ) {
-                return;
-            }
-
-            // Get visitor's member types - uses false to get multiple types
-            $visitors_member_types = bp_get_member_type( get_current_user_id(), false );
-
-            // Add the meta query in order to fetch only the appropriate notifications
-            $meta_query_args = '';
-            if ( is_array( $visitors_member_types ) && count( $visitors_member_types ) > 0 ) {
-                $meta_query_args = array(
-                    'relation' => 'OR',
-
-                );
-                foreach ( $visitors_member_types as $member_type ) {
-                    $meta_query_args[] = array(
-                        'key'     => 'notice-blocked-member-types',
-                        'value'   => $member_type,
-                        'compare' => 'NOT LIKE'
-                    );
-                }
-
-                /**
-                 * Do this for notices that have no blocked types
-                 */
-                $meta_query_args[] = array(
-                    'key'     => 'notice-blocked-member-types',
-                    'compare' => 'NOT EXISTS'
-                );
-
-            }
-
-
-            // Get member's read notifications
-            $read_notification_ids = get_user_meta( get_current_user_id(), SELF::READ_META_KEY, true );
-
-            if ( empty( $read_notification_ids ) ) {
-                $read_notification_ids = array();
-            }
-
-            /**
-             * Add sorting filters
-             */
-            $order = array(
-                'orderby' => 'date',
-                'order'   => 'ASC',
-            );
-
-            // Filter the order of the announcements
-            $order = apply_filters( 'bp-block-member-posting-query-order', $order );
-
-            // Get notifications
-            $args = array(
-                'post_type'      => 'pinned_feed_notices',
-                'posts_per_page' => - 1,
-                'orderby'        => $order['orderby'],
-                'order'          => $order['order'],
-            );
-
-            // Remove notifications that the member has already read
-            if ( count( $read_notification_ids ) >= 1 ) {
-                $args['post__not_in'] = $read_notification_ids;
-            }
-
-            if ( ! empty( $meta_query_args ) ) {
-                $args['meta_query'] = $meta_query_args;
-            }
-
-            // Run the query
-            $the_query = new WP_Query( $args );
-
-            // If there is an error return false
-            if ( is_wp_error( $the_query ) ) {
-                return false;
-            }
-
-            // Return an array of post objects
-            $notifications = $the_query->posts;
-
-            // If there are no notifications bail out.
-            if ( count( $notifications ) <= 0 ) {
-                return false;
-            }
-            ?>
-			<ul class="bp-pinned-feed-notice-wrapper">
-                <?php
-                foreach ( $notifications as $notification ) {
-                    ?>
-					<li class="bp-pinned-feed-notice">
-						<span class="remove-notification" data-notif-id="<?php echo absint( $notification->ID ); ?>"
-							  title="<?php _e( 'Remove this message', 'bp-block-member-posting' ); ?>">x</span>
-                        <?php
-                        $content = wp_kses_post( $notification->post_content );
-                        echo apply_filters( 'the_content', $content );
-                        ?>
-					</li>
-                    <?php
-                }
-                ?>
-
-			</ul>
-
-            <?php
-        }
-
-        /**
-         * Setup AJAX callback for removing feed notices
-         *
-         */
-        public function delete_pinned_feed_notice() {
-
-            $nonce                 = sanitize_text_field( $_POST['nonce'] );
-            $notification_id       = (int) sanitize_text_field( $_POST['notifID'] );
-            $meta_key              = SELF::READ_META_KEY;
-            $removed_notifications = get_user_meta( get_current_user_id(), $meta_key, true );
-
-            if ( empty( $removed_notifications ) ) {
-                $removed_notifications = array();
-            }
-
-
-            // Security Validate Nonce
-            if ( ! wp_verify_nonce( $nonce, 'ajax_nonce' ) ) {
-                $response['success'] = false;
-                $response['content'] = 'Security check failed';
-                echo json_encode( $response );
-                wp_die();
-            }
-
-            // Security Validate Nonce
-            if ( $notification_id <= 0 ) {
-                $response['success'] = false;
-                $response['content'] = 'Problem with the notification ID';
-                echo json_encode( $response );
-                wp_die();
-            }
-
-            if ( in_array( $notification_id, $removed_notifications ) ) {
-                $response['success'] = false;
-                $response['content'] = 'Notification already deleted';
-                echo json_encode( $response );
-                wp_die();
-            }
-
-            // Add the new notification to the deleted list
-            $removed_notifications[] = $notification_id;
-
-            // Update deleted notifications meta
-            $result = update_user_meta( get_current_user_id(), $meta_key, $removed_notifications );
-
-            $response['success'] = true;
-            $response['content'] = $result;
-
-
-            echo json_encode( $response );
-            exit;
-
-        }
     }
 
     new BP_Block_Member_Posting();
